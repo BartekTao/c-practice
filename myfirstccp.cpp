2045bea4508629e7d398cccf2afd877aac805ac4
BartekTao <pony830906@gmail.com>
2021-05-14 23:32:22 +0800

    findRepeatNumber

--- myfirstccp.cpp
+++ myfirstccp.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        map<int, bool> checkRepeat;
+        for(int i = 0; i < nums.size(); i++){
+            if(checkRepeat[nums[i]])
+                return nums[i];
+            else{
+                checkRepeat[nums[i]] = true;
+            }
+        }
+        return -1;
+    }
+
+    int maxArea(vector<int>& height) {
+        for(int i = 1; i < height.size(); i++){
+            
+        }
+        return -1;
+    }
+};
 
 int main(){
-    using namespace std;
-    cout << "Come up and C++ me some time.";
+    vector<int> nums { 1, 2, 3, 1 };
+
+    Solution s;
+
+    int result = s.findRepeatNumber(nums);
+    cout << result;
     cout << endl;
-    cout << "You won! Target it!";
 }
+
+
+
