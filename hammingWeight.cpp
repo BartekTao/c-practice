eb5f350824a273366c3b4910a72159e2f5ff84d7
BartekTao <pony830906@gmail.com>
2021-06-19 18:35:23 +0800

    191. Number of 1 Bits

---
+++ hammingWeight.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int hammingWeight(uint32_t n) {
+    int ans = 0;
+    while(n){
+        if(n & 1){
+            ans++;
+        }
+        n >>= 1;
+    }
+    return ans;
+}
