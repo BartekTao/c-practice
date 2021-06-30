3439e2dcac0dd761ea45337cbc4400cc4a1160bd
BartekTao <pony830906@gmail.com>
2021-06-30 22:53:21 +0800

    138. Copy List with Random Pointer

--- copyRandomList.cpp
+++ copyRandomList.cpp
@@ -17,7 +17,8 @@ public:
     }
 };
 
-Node* copyRandomList(Node* head) {
+//方法一: hashtable
+/*Node* copyRandomList(Node* head) {
     if(!head) return {};
     unordered_map<Node*, Node*> node_map;
     Node* curr = head;
@@ -32,4 +33,40 @@ Node* copyRandomList(Node* head) {
         curr = curr->next;
     }
     return node_map[head];
+}*/
+
+//方法二: 從舊有node變形複製
+Node* copyRandomList(Node* head) {
+    //每個節點後面複製前一個節點
+    // A-> A'-> B-> B'-> C-> C'
+    Node *curr = head;
+    while (curr)
+    {
+        Node *temp = curr->next;
+        curr->next = new Node(curr->val);
+        curr->next->next = temp;
+        curr = curr->next->next;
+    }
+
+    //建立關聯-random
+    curr = head;
+    while (curr)
+    {
+        if(curr->random){
+            curr->next->random = curr->random->next;
+        }
+        curr = curr->next->next;
+    }
+    //重建關聯-next
+    Node* ans = head->next;
+    curr = head;
+    Node* newCurr = head->next;
+    while(curr){
+        curr->next = curr->next->next;
+        if(newCurr->next)
+            newCurr->next = newCurr->next->next;
+        curr = curr->next;
+        newCurr = newCurr->next;
+    }
+    return ans;
 }
